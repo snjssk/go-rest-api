47bb44733233dfad4378695abf92724f1432ad4d
snjssk <snjssk0403@gmail.com>
2020-06-22 23:54:00 +0900

    初期ファイルを追加

--- go_rest_api.go
+++ go_rest_api.go
@@ -1,8 +1,26 @@
 // package go_rest_api
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main()  {
-	fmt.Println("Hello")
+	// データの取得
+	// ioutilはバイト文字で返ってくるため、stringでキャストする
+	//resp, _ := http.Get("https://www.google.co.jp/")
+	//defer resp.Body.Close()
+	//body, _ := ioutil.ReadAll(resp.Body)
+	//fmt.Println(string(body))
+
+	// URL が正しいものかの判定
+	//base, _ := url.Parse("http://example.com/aaa")
+	//reference, _ := url.Parse("/test?a=b")
+	//endpoint := base.ResolveReference(reference)
+	//fmt.Println(endpoint)
+
+	// エラーが返ってくるため、log.Fatal()を利用する
+	log.Fatal(http.ListenAndServe(":18080", nil))
+
 }
